a1b3073043b9b0465e48d9b2035fb08e62e5fa35
shhanky007 <shhanky007@gmail.com>
2020-04-12 00:13:11 +0530

    updated code with dummy list

--- CalenderTask/project/Screens/Screen3.js
+++ CalenderTask/project/Screens/Screen3.js
@@ -1,15 +1,115 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, SectionList} from 'react-native';
 
 import Header from '../components/Header';
 import DaysList from '../components/Day';
+import {FlatList, ScrollView} from 'react-native-gesture-handler';
+
+const DummyData = [
+  {
+    day: 'Sun',
+    month: 'Apr',
+    date: '12',
+    data: {
+      eventValue: 'Ester',
+    },
+  },
+  {
+    day: 'Mon',
+    month: 'Apr',
+    date: '13',
+    data: {
+      eventValue: 'Yoga Day',
+    },
+  },
+  {
+    day: 'Tue',
+    month: 'Apr',
+    date: '14',
+    data: {
+      eventValue: 'Jayanti',
+    },
+  },
+  {
+    day: 'Friday',
+    month: 'June',
+    date: '23',
+    data: {
+      eventValue: 'Shikara',
+    },
+  },
+  {
+    day: 'Sun',
+    month: 'July',
+    date: '25',
+    data: {
+      eventValue: 'Test data',
+    },
+  },
+  {
+    day: 'Wed',
+    month: 'Aug',
+    date: '15',
+    data: {
+      eventValue: 'Independence Day',
+    },
+  },
+  {
+    day: 'Sun',
+    month: 'Apr',
+    date: '12',
+    data: {
+      eventValue: 'Ester',
+    },
+  },
+  {
+    day: 'Mon',
+    month: 'Apr',
+    date: '13',
+    data: {
+      eventValue: 'Yoga Day',
+    },
+  },
+  {
+    day: 'Tue',
+    month: 'Apr',
+    date: '14',
+    data: {
+      eventValue: 'Jayanti',
+    },
+  },
+  {
+    day: 'Friday',
+    month: 'June',
+    date: '23',
+    data: {
+      eventValue: 'Shikara',
+    },
+  },
+  {
+    day: 'Sun',
+    month: 'July',
+    date: '25',
+    data: {
+      eventValue: 'Test data',
+    },
+  },
+  {
+    day: 'Wed',
+    month: 'Aug',
+    date: '15',
+    data: {
+      eventValue: 'Independence Day',
+    },
+  },
+];
 
 class Screen3 extends Component {
   constructor(props) {
     super(props);
     this.state = {
       MonthValue: this.props.route.params.SelectedDay.month,
-      ClickedModalPlus:false
+      ClickedModalPlus: false,
     };
     this.handleHeaderValueChangte = this.handleHeaderValueChangte.bind(this);
     this.handlePlusClick = this.handlePlusClick.bind(this);
@@ -18,9 +118,9 @@ class Screen3 extends Component {
   handleHeaderValueChangte = Num => {
     this.setState({MonthValue: Num});
   };
-  handlePlusClick=()=>{
-    this.setState({ClickedModalPlus:!this.state.ClickedModalPlus})
-  }
+  handlePlusClick = () => {
+    this.setState({ClickedModalPlus: !this.state.ClickedModalPlus});
+  };
 
   render() {
     const {SelectedDay} = this.props.route.params;
@@ -36,7 +136,6 @@ class Screen3 extends Component {
           isWeekShow={false}
           isAddButtonVisible={true}
           heightPerc={0.2}
-
           PlusOnPress={this.handlePlusClick}
           // Children={<DaysList
           //   SelectedDate={SelectedDay}
@@ -55,7 +154,7 @@ class Screen3 extends Component {
               width: 0,
               height: 5,
             },
-            height:100,
+            height: 100,
             shadowOpacity: 0.5,
           }}>
           <DaysList
@@ -64,6 +163,47 @@ class Screen3 extends Component {
             ModelState={this.state.ClickedModalPlus}
             onPressGet={this.handlePlusClick}
           />
+        </View>
+        <View style={{top: 80}}>
+          <ScrollView style={{}}>
+            {DummyData.map(item => {
+              return (
+                <View>
+                  <View>
+                    <Text
+                      style={{
+                        fontWeight: 'bold',
+                        fontSize: 18,
+                        paddingLeft: 8,
+                      }}>
+                      {item.day},{(item.month)} {item.date}
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      backgroundColor: 'white',
+                      flex: 1,
+                      height: 45,
+                      paddingVertical: 2,
+                      alignItems: 'center',
+                    }}>
+                    <View
+                      style={{
+                        flex: 0.2,
+                        borderLeftColor: 'purple',
+                        borderRightWidth: 1,
+                      }}>
+                      <Text style={{padding: 8}}>All-day</Text>
+                    </View>
+                    <Text style={{flex: 0.8, padding: 8}}>
+                      {item.data.eventValue}
+                    </Text>
+                  </View>
+                </View>
+              );
+            })}
+          </ScrollView>
         </View>
       </View>
     );
